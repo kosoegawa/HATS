ffa9625991753d3297972cb414cd6e8fa41f7791
Kazutoyo Osoegawa <kazutoyo@stanford.edu>
2022-07-06 17:33:13 -0700

    Updated ASSIGNED_SHORT.pm

--- SEROTYPE/ASSIGNED_SHORT.pm
+++ SEROTYPE/ASSIGNED_SHORT.pm
@@ -7,7 +7,7 @@
 
 # module: ASSIGNED_SHORT.pm 
 # This module was developed to print table
-# last modified and documented on January 24 2022
+# last modified and documented on May 28 2022
 
 package ASSIGNED_SHORT;
 use strict;
@@ -40,13 +40,13 @@ $antigen{ "B-4501" } = "B45";
 $antigen{ "B-1302" } = "B13";
 $antigen{ "B-1401" } = "B64";
 $antigen{ "B-1402" } = "B65";
-$antigen{ "B-1501" } = "B62";
+#$antigen{ "B-1501" } = "B62";
 #$antigen{ "B-1517" } = "B63";
 $antigen{ "B-1502" } = "B75";
 $antigen{ "B-1512" } = "B76";
 $antigen{ "B-1514" } = "B76";
 $antigen{ "B-1513" } = "B77";
-$antigen{ "B-3801" } = "B38";
+#$antigen{ "B-3801" } = "B38";
 $antigen{ "B-3901" } = "B3901";
 $antigen{ "B-3902" } = "B3902";
 $antigen{ "B-5701" } = "B57";
@@ -67,7 +67,7 @@ $antigen{ "B-4701" } = "B47";
 $antigen{ "B-5301" } = "B53";
 $antigen{ "B-5901" } = "B59";
 $antigen{ "B-6701" } = "B67";
-$antigen{ "B-1510" } = "B71";
+#$antigen{ "B-1510" } = "B71";
 $antigen{ "B-1503" } = "B72";
 $antigen{ "B-7301" } = "B73";
 $antigen{ "B-7801" } = "B78";
@@ -87,6 +87,8 @@ $antigen{ "DR-0701" } = "DR7";
 $antigen{ "DR-0901" } = "DR9";
 $antigen{ "DR-1001" } = "DR10";
 $antigen{ "DR-0404" } = "DR-0401";
+$antigen{ "DR-1305" } = "DR-1303";	#added
+$antigen{ "DR-1419" } = "DR-1402";	#added
 
 $antigen{ "DQ-0201" } = "DQ2";
 $antigen{ "DQ-0301" } = "DQ7";
@@ -98,25 +100,25 @@ $antigen{ "DQ-0501" } = "DQ5";
 $antigen{ "DQ-0602" } = "DQ6";
 $antigen{ "DQ-0604" } = "DQ6";
 
-$antigen{"DP0101"} = "DP01";
-$antigen{"DP0201"} = "DP0201";
-$antigen{"DP0301"} = "DP03";
-$antigen{"DP0401"} = "DP0401";
-$antigen{"DP1001"} = "DP10";
-$antigen{"DP1501"} = "DP15";
-$antigen{"DP1801"} = "DP18";
-$antigen{"DP4601"} = "DP46";
-$antigen{"DP0402"} = "DP0402";
-$antigen{"DP0202"} = "DP0202";
-$antigen{"DP0601"} = "DP06";
-$antigen{"DP1301"} = "DP13";
-$antigen{"DP4501"} = "DP45";
-$antigen{"DP8001"} = "DP80";
+$antigen{"DP0101"} = "DP-01";
+$antigen{"DP0201"} = "DP-0201";
+$antigen{"DP0301"} = "DP-03";
+$antigen{"DP0401"} = "DP-0401";
+$antigen{"DP1001"} = "DP-10";
+$antigen{"DP1501"} = "DP-15";
+$antigen{"DP1801"} = "DP-18";
+$antigen{"DP4601"} = "DP-46";
+$antigen{"DP0402"} = "DP-0402";
+$antigen{"DP0202"} = "DP-0202";
+$antigen{"DP0601"} = "DP-06";
+$antigen{"DP1301"} = "DP-13";
+$antigen{"DP4501"} = "DP-45";
+$antigen{"DP8001"} = "DP-80";
 
-$antigen{"DP1101"} = "DP01"; 
-$antigen{"DP3401"} = "DP15"; 
-$antigen{"DP13601"} = "DP45";  
-$antigen{"DP6901"} = "DP03";
+$antigen{"DP1101"} = "DP-01"; 
+$antigen{"DP3401"} = "DP-15"; 
+$antigen{"DP13601"} = "DP-45";  
+$antigen{"DP6901"} = "DP-03";
 
 my @broad = ("A9","A10","A19","A28","B5","B12","B14","B15","B16","B17","B21","B22","B40","B70","Cw3","DR2","DR3","DR5","DR6","DQ1","DQ3");
 
@@ -230,6 +232,9 @@ sub COMBINED {
 					if (( $serotype eq "A-0305" ) && ( $allele =~ /A\*11/ )) {	# request from MFV on February 10 2021
 						print FILE $allele . "," . $lax . "," . $serotype . ",A11,A11";
 					}
+					elsif (( $serotype eq "B-4406" ) && ( $allele =~ /B\*51/ )) {	# request from MFV on February 10 2021
+						print FILE $allele . "," . $lax . "," . $serotype . ",B51,B5";
+					}
 					else {
 						print FILE $allele . "," . $lax . "," . $serotype . "," . $base_ref->{ $group } . "," . $broad_ref->{ $group };
 					}
@@ -272,6 +277,9 @@ sub COMBINED {
 				my $full = ",FULL,";
 				if (( $serotype eq "A-0305" ) && ( $allele =~ /A\*11/ )) {	# request from MFV on February 10 2021
 					print FILE $allele . $full . $serotype . ",A11,A11";
+				}
+				elsif (( $serotype eq "B-4406" ) && ( $allele =~ /B\*51/ )) {	# request from MFV on February 10 2021
+					print FILE $allele . "," . $full . "," . $serotype . ",B51,B5";
 				}
 				else {
 					print FILE $allele . $full  . $serotype . "," . $base_ref->{ $group } . "," . $broad_ref->{ $group };
@@ -519,7 +527,7 @@ sub COMBINED {
 
 			}
 			else {
-				print FILE ",UA,None,None,None";
+				print FILE ",UNA,None,None,None";
 				if ( exists $ciwd_ref->{ $twoField } ) {
 					print FILE "," . $ciwd_ref->{ $twoField} . ",";
 				}
@@ -569,15 +577,19 @@ sub COMBINED_TWO {
 
 	open(FILE, ">output/" . $gene . "_TwoField_Serotype_Table_IMGT_HLA_" . $database . "_" . $date . ".csv");
 	if (( $gene eq "B" ) || ( $gene eq "C" )) {
-		print FILE "Allele,COMMENT,Serotype,WHOAccepted,Broad,CIWD3.0,CWD2.0,EURCWD,Bw4/Bw6,C1/C2\n";
+		print FILE "Allele,Serotype,Qualifiers,COMMENT,WHO Assigned,Broad,CIWD3.0,CWD2.0,EURCWD,Bw4/Bw6,C1/C2\n";
+	}
+	elsif ( $gene eq "A" ) {
+		print FILE "Allele,Serotype,Qualifiers,COMMENT,WHO Assigned,Broad,CIWD3.0,CWD2.0,EURCWD,Bw4/Bw6\n";
 	}
 	else {
-		print FILE "Allele,COMMENT,Serotype,WHOAccepted,Broad,CIWD3.0,CWD2.0,EURCWD,Bw46C12DR5X\n";
+		print FILE "Allele,Serotype,Qualifiers,COMMENT,WHO Assigned,Broad,CIWD3.0,CWD2.0,EURCWD,DR5X\n";
 	}
 	my %twoField;
 
 	foreach my $allele ( @$alleles_sorted_ref ) {		#go through all alleles
 		my $twoField = "";
+		my $qualifier = "I";
 		unless (( exists $nullAllele_ref->{ $allele } ) || ( exists $qallele_ref->{ $allele })) {		# Null
 			if ( $allele =~ /(\w+\*\d+:\d+)/ ) {
 				$twoField = $1;
@@ -606,19 +618,10 @@ sub COMBINED_TWO {
 				}
 				if ( $lax eq "LAX" ) {
 					$lax = "SEROTYPE";
+					$qualifier = "S";
 				}
 				if ( exists $cross_ref->{ $allele } ) {		# cross-reactivity
-					print FILE $twoField . "," . $lax . "_C," . $serotype . "," . $who . ",";
-					my $test_broad = 0;
-					foreach my $broad ( @broad ) {
-						if ( $broad_ref->{ $group } eq $broad ) {
-							print FILE $broad;
-							$test_broad = 1;
-						}
-					}
-					if ( $test_broad == 0 ) {
-						print FILE "";
-					}
+					print FILE $twoField . "," . $serotype . "," . $qualifier . "," . $lax . "_C," . $who . "," . $broad_ref->{ $group };
 					my $cross = "";
 					for ( my $index = 0; $index < scalar @{$cross_ref->{ $allele }}; $index++ ) {
 						if ( $index == 0 ) {
@@ -659,20 +662,13 @@ sub COMBINED_TWO {
 				}
 				else {	# no cross-reactive
 					if (( $serotype eq "A-0305" ) && ( $allele =~ /A\*11/ )) {	# request from MFV on February 10 2021
-						print FILE $twoField . "," . $lax . "," . $serotype . ",A11,";
+						print FILE $twoField . "," . $serotype . "," . $qualifier . "," . $lax . ",A11,A11";
+					}
+					elsif (( $serotype eq "B-4406" ) && ( $allele =~ /B\*51/ )) {	# request from MFV on February 10 2021
+						print FILE $twoField . "," . $serotype . "," . $qualifier . "," . $lax . ",B51,B5";
 					}
 					else {
-						print FILE $twoField . "," . $lax . "," . $serotype . "," . $who . ",";
-						my $test_broad = 0;
-						foreach my $broad ( @broad ) {
-							if ( $broad_ref->{ $group } eq $broad ) {
-								print FILE $broad;
-								$test_broad = 1;
-							}
-						}
-						if ( $test_broad == 0 ) {
-							print FILE "";
-						}
+						print FILE $twoField . "," . $serotype . "," . $qualifier . "," . $lax . "," . $who . "," . $broad_ref->{ $group };
 					}
 					if ( exists $ciwd_ref->{ $twoField } ) {
 						print FILE "," . $ciwd_ref->{ $twoField};
@@ -723,21 +719,15 @@ sub COMBINED_TWO {
 					$who = $base_ref->{ $group };
 				}
 				my $full = ",FULL,";
+				$qualifier = "F";
 				if (( $serotype eq "A-0305" ) && ( $allele =~ /A\*11/ )) {	# request from MFV on February 10 2021
-					print FILE $twoField . $full . $serotype . ",A11,";
+					print FILE $twoField . "," . $serotype . "," . $qualifier . $full . "A11,A11";
+				}
+				elsif (( $serotype eq "B-4406" ) && ( $allele =~ /B\*51/ )) {	# request from MFV on February 10 2021
+					print FILE $twoField . "," . $serotype . "," . $qualifier . $full . "B51,B5";
 				}
 				else {
-					print FILE $twoField . $full  . $serotype . "," . $who . ",";
-					my $test_broad = 0;
-					foreach my $broad ( @broad ) {
-						if ( $broad_ref->{ $group } eq $broad ) {
-							print FILE $broad;
-							$test_broad = 1;
-						}
-					}
-					if ( $test_broad == 0 ) {
-						print FILE "";
-					}
+					print FILE $twoField . "," . $serotype . "," . $qualifier . $full . $who . "," . $broad_ref->{ $group };
 				}
 				if ( exists $ciwd_ref->{ $twoField } ) {
 					print FILE "," . $ciwd_ref->{ $twoField};
@@ -805,30 +795,10 @@ sub COMBINED_TWO {
 				}
 				if ( $test == 1 ) {
 					if ( $num == 1 ) {	# specific short
-						print FILE "S," . $serotype . "," . $who . ",";
-						my $test_broad = 0;
-						foreach my $broad ( @broad ) {
-							if ( $broad_ref->{ $group } eq $broad ) {
-								print FILE $broad . ",";
-								$test_broad = 1;
-							}
-						}
-						if ( $test_broad == 0 ) {
-							print FILE ",";
-						}
+						print FILE $serotype . "," . $qualifier . ",SHORT," . $who . "," . $broad_ref->{ $group } . ",";
 					}
 					else {		# cross-reactive short
-						print FILE "SC," . $serotype . "," . $who . ",";
-						my $test_broad = 0;
-						foreach my $broad ( @broad ) {
-							if ( $broad_ref->{ $group } eq $broad ) {
-								print FILE $broad . ",";
-								$test_broad = 1;
-							}
-						}
-						if ( $test_broad == 0 ) {
-							print FILE ",";
-						}
+						print FILE $serotype . "," . $qualifier . ",SHORT-CROSSREACTIVE," . $who . "," . $broad_ref->{ $group } . ",";
 					}
 				}
 				else {		# no match => capture the first element
@@ -846,30 +816,10 @@ sub COMBINED_TWO {
 						}
 					}
 					if ( $num == 1 ) {	# specific short
-						print FILE "S," . $serotype . "," . $who . ",";
-						my $test_broad = 0;
-						foreach my $broad ( @broad ) {
-							if ( $broad_ref->{ $group } eq $broad ) {
-								print FILE $broad . ",";
-								$test_broad = 1;
-							}
-						}
-						if ( $test_broad == 0 ) {
-							print FILE ",";
-						}
+						print FILE $serotype . "," . $qualifier . ",SHORT," . $who . "," . $broad_ref->{ $group } . ",";
 					}
 					else {		# cross-reactive short
-						print FILE "SC," . $serotype . "," . $who . ",";
-						my $test_broad = 0;
-						foreach my $broad ( @broad ) {
-							if ( $broad_ref->{ $group } eq $broad ) {
-								print FILE $broad . ",";
-								$test_broad = 1;
-							}
-						}
-						if ( $test_broad == 0 ) {
-							print FILE ",";
-						}
+						print FILE $serotype . "," . $qualifier . ",SHORT-CROSSREACTIVE," . $who . "," . $broad_ref->{ $group } . ",";
 					}
 				}
 
@@ -991,22 +941,22 @@ sub COMBINED_TWO {
 			( $allele =~ /DQB1\*05:03:02/ ) || ( $allele =~ /DQB1\*06:01:02/ ) || ( $allele =~ /DQB1\*06:05:02/ ) ||
 			( $allele =~ /DQB1\*06:06/ )) {	# missing key residues 9 - 14
 				if ( $allele =~ /DRB1\*08:04:02/ ) {
-					print FILE ",InSilico,DR-0801,DR8,";
+					print FILE ",DR-0801,F,InSilico,DR8,DR8,";
 				}
 				elsif ( $allele =~ /DRB1\*04:20/ ) {	# missing key residues 9 - 14
-					print FILE ",InSilico,DR-0403,DR4,";
+					print FILE ",DR-0403,S,InSilico,DR4,DR4";
 				}
 				elsif ( $allele =~ /DQB1\*05:03:02/ ) {
-					print FILE ",InSilico,DQ5,DQ5,DQ1";
+					print FILE ",DQ5,S,InSilico,DQ5,DQ1";
 				}
 				elsif ( $allele =~ /DQB1\*06:01:02/ ) {
-					print FILE ",InSilico,DQ6,DQ6,DQ1";
+					print FILE ",DQ6,F,InSilico,DQ6,DQ1";
 				}
 				elsif ( $allele =~ /DQB1\*06:05:02/ ) {
-					print FILE ",InSilico,DQ6,DQ6,DQ1";
+					print FILE ",DQ6,F,InSilico,DQ6,DQ1";
 				}
 				elsif ( $allele =~ /DQB1\*06:06/ ) {
-					print FILE ",InSilico,DQ6,DQ6,DQ1";
+					print FILE ",DQ6,S,InSilico,DQ6,DQ1";
 				}
 				if ( exists $ciwd_ref->{ $twoField } ) {
 					print FILE "," . $ciwd_ref->{ $twoField} . ",";
@@ -1029,7 +979,7 @@ sub COMBINED_TWO {
 
 			}
 			else {
-				print FILE ",UA,None,None,None";
+				print FILE ",None,U,UNA,None,None";
 				if ( exists $ciwd_ref->{ $twoField } ) {
 					print FILE "," . $ciwd_ref->{ $twoField} . ",";
 				}
